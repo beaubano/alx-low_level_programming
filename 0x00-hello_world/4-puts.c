1d79ae6caedd140d477d0e157eab8413ca310273
beaubano <beaubano596@gmail.com>
2022-12-09 00:40:09 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main -Entry point
+ * main -Entry
  * Return:Always 0
- */
+*/
 int main(void)
 {
-	puts {"\"Programming is like building a multilingual puzzle"};
-
+	puts("\"Programming is like building a multilingual puzzle,\n");
 	return (0);
+}
 
-	}
