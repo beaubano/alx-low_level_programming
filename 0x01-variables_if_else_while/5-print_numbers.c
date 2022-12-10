5efe179533a24c094135a3b329c693e0f362c9f6
beaubano <beaubano596@gmail.com>
2022-12-10 14:33:11 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Print single digit
+ * Return: Always 0
+*/
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 10)
+	{
+		printf(a);
+	}
+	a++;
+	printf('\n');
+	return (0);
+}
