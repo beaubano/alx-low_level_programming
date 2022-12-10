dd1e3fd121de45a3dfed45011afef9778a3fef7b
beaubano <beaubano596@gmail.com>
2022-12-10 11:41:42 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -main function to print the alphabet lowercase and uppercase
+ * Return: Always 0
+ *
+*/
+int main(void)
+{
+	int n = 'a';
+	int m = 'A';
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	for (m = 'A'; m <= 'Z'; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (o);
+}
