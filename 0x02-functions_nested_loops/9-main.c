833ebcdd0413b481c1a0f5d7042ef92e77e4a93b
beaubano <beaubano596@gmail.com>
2022-12-24 06:19:24 -0800

    table

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code 
+ * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
+
 int main(void)
+
 {
-	time_teble();
-	return(0);
+	times_table();
+	return (0);
 }
