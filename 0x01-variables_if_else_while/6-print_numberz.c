4cad3714bde4a6ddb68df8a72a4d278fe993446c
beaubano <beaubano596@gmail.com>
2023-01-01 05:29:53 -0800

    print digits

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main -main function to print single digit
-*
-* Return: Always 0
-*
+ * main - Prints numbers.
+ *
+ * Return: Always 0 (Success)
+ *
 */
-
 int main(void)
-
 {
-	int a = 0;
-
-	while (a < 10)
-	{
-
-		putchar (('a + 10') + 0);
-		a++;
-
-	}
-
+int num;
+for (num = 0; num <= 9; num++)
+{
+	putchar((num % 10) + '0');
+}
 	putchar('\n');
 	return (0);
-
 }
