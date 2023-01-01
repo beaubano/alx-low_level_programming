7bbfc70f88234fdb3d893ae4485ff5c89b1ec2e0
beaubano <beaubano596@gmail.com>
2023-01-01 04:13:17 -0800

    print all digit number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - main function to print single digit 
- * Return: Always 0
- */
-int main(void)
-{
+* main -main function to print single digit
+*
+* Return: Always 0
+*
+*/
 
+int main(void)
+
+{
 	int a = 0;
 
 	while (a < 10)
 	{
+
 		putchar (('a + 10') + 0);
 		a++;
+
 	}
+
 	putchar('\n');
 	return (0);
 
