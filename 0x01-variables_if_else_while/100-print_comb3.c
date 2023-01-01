41bbbfebee1fc43933d6549b12e76f5ff4dc35b0
beaubano <beaubano596@gmail.com>
2023-01-01 07:26:26 -0800

    prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,16 +9,16 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 99 ; num++)
+	for (num = 0; num < 99 ; num++)
 	{
 		putchar((num / 10) + '0');
 		putchar((num % 10) + '0');
-	}
 		if (num != 99)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
