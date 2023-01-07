f22f84f9963297c5b8e96fbf4b918f9d96794680
beaubano <beaubano596@gmail.com>
2023-01-07 11:08:47 -0800

    thout libraries what have we? We have no past and no future# Please enter the commit message for your changes. Lines starting

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,12 +5,25 @@
  * @str: char
  * Return: the integer length
  */
-int strlen_rec(char *str)
+int find_strlen(char *s)
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
+/**
+ * find_strlen-Return the length of a string 
+ * @s:the string to be measured.
+ * Return: the length of the string 
+ */
+
+int find_strlen(char)
 {
-	if (*str)
+	int len = 0;
+
+	if (*(s + len))
+
 	{
-		str++;
-		return (1 + strlen_rec(str));
+		index++;
+		return(len);
 	}
 		return (0);
 }
